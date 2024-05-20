43a891c141c7528e2d567da899cc1bbd92ddffbb
Angelo Santos <gabrielange883@gmail.com>
2024-05-20 13:57:34 -0300

    trycatch

---
+++ CSharp/CSharp/Especial/Exceptions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Especial
+{
+    class Exceptions
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                String file = @"C:\\Users\\gabri\\Downloads\\PaperCshap.txt";
+                using (StreamReader sr = new StreamReader(file))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+
+                    }
+                }
+
+            } catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("file not found");
+            }
+            finally
+            {
+                Console.WriteLine("Loading file");
+            }
+
+        }
+    }
+}
--- CSharp/CSharp/obj/Debug/net8.0/CSharp.AssemblyInfo.cs
+++ CSharp/CSharp/obj/Debug/net8.0/CSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88938092f4cc280dd1a75e033818483c630f877b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
