1b72c93dba5a74c3412b81da6f0d33ff93ffe8c4
Angelo Santos <gabrielange883@gmail.com>
2024-05-26 12:34:59 -0300

    Generics

---
+++ CSharp/CSharp/Especial/Generic.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Especial
+{
+    class Generic
+    {
+        static void Main(string[] args)
+        {
+            Box<String> toy = new Box<String>();
+            toy.addValue("Max steel");
+            toy.addValue("BARBIE");
+            toy.showValues();
+        }
+    }
+    class Box<T>
+    {
+        private List<T> _list = new List<T>();
+
+        public void addValue(T value)
+        {
+            _list.Add(value);
+        }
+
+        public void showValues()
+        {
+            foreach(T value in _list)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
